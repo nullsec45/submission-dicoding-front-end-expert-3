7a596ec7ae957e5d66e54dd523db9f375e57eec5
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-26 17:28:09 +0700

    index js

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import 'regenerator-runtime';
+import '../styles/main.scss';
+import swRegister from './utils/sw-register';
+import App from './views/app';
+
+const app = new App({
+    hamburgerButton: document.querySelector('#hamburgerIcon'),
+    content: document.querySelector('#mainContent'),
+    drawer: document.querySelector("#navbarResponsive"),
+    hero: document.querySelector("#hero"),
+    mainContent: document.querySelector("#mainContent")
+});
+
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+});
+
+window.addEventListener('load', () => {
+    app.renderPage();
+    swRegister();
+});
