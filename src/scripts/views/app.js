d3e66730edc1cc145a37907574fe4ecd087a237d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-26 17:29:02 +0700

    views

---
+++ src/scripts/views/app.js
@@ -0,0 +1,36 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+import UrlParser from '../routes/url-parser';
+import routes from '../routes/routes';
+
+class App {
+    constructor({ hamburgerButton, content, drawer, hero, mainContent }) {
+        this._hero = hero;
+        this._mainContent = mainContent;
+        this._content = content;
+        this._initialAppShell({ hamburgerButton, drawer });
+    }
+
+    _initialAppShell(element) {
+        DrawerInitiator.init({
+            button: element.hamburgerButton,
+            drawer: element.drawer
+        });
+
+        // kita bisa menginisiasikan komponen lain bila ada
+    }
+
+    async renderPage() {
+        const url = UrlParser.parseActiveUrlWithCombiner();
+        if (url !== "/") {
+            this._hero.style.display = "none";
+        } else {
+            this._hero.style.display = "flex";
+        }
+        const page = routes[url];
+        this._content.innerHTML = "";
+        this._content.insertAdjacentHTML(`afterbegin`, await page.render());
+        await page.afterRender(this._mainContent);
+    }
+}
+
+export default App;
