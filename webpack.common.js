636c7127ac42a2a26b0d77c963246e4a0675c512
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-01 00:46:47 +0700

    BundleAnalyzerPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,31 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
-  // resolve: {
-  // fallback: {
-  //   "path": require.resolve("path-browserify"),
-  //   "util": require.resolve("util"),
-  //   "assert": require.resolve("assert"),
-  //   "constants": require.resolve("constants-browserify"),
-  //   "os": require.resolve("os-browserify/browser"),
-  //   "https": require.resolve("https-browserify"),
-  //   "http": require.resolve("stream-http"),
-  //   "querystring": require.resolve("querystring-es3"),
-  //   "url": require.resolve("url/"),
-  //   "vm": require.resolve("vm-browserify"),
-  //   "zlib": require.resolve("browserify-zlib"),
-  //   "crypto": require.resolve("crypto-browserify")
-  // }
-  // },
-  // externals: {
-  //   sharp: 'commonjs sharp'
-  // },
   output: {
     filename: '[name].bundle.js',
     clean: true,
@@ -51,20 +33,52 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
     new CleanWebpackPlugin(),
+
+    new BundleAnalyzerPlugin(),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
+
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+            ignore: ['**/images/heros**'],
+          },
         },
       ],
     }),
+
     new WorkboxWebpackPlugin.InjectManifest({
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
